6b9b9b6bc4a65d7c76a671846e60d7d832ab69c7
Terrence Drozdowski <terry@pulsar.local>
2013-09-04 16:23:53 -0700

    Initial commit

---
+++ app/controllers/GridFSController.scala
@@ -0,0 +1,46 @@
+package controllers
+
+import play.api.Play.current
+import play.api.mvc.{Action, Controller}
+import play.modules.reactivemongo.MongoController
+
+import reactivemongo.bson._
+import reactivemongo.api._
+import reactivemongo.api.gridfs._
+import reactivemongo.api.gridfs.Implicits._
+import reactivemongo.bson.DefaultBSONHandlers._
+
+import scala.concurrent.Future
+
+import play.modules.reactivemongo.json.collection.JSONCollection
+import play.modules.reactivemongo.json.BSONFormats
+
+import play.api.libs.json.{JsValue, Json}
+
+/**
+ * Created by terry on 9/4/13.
+ */
+object GridFSController extends Controller with MongoController {
+
+  def users = db.collection[JSONCollection]("users")
+  val gridFS = new reactivemongo.api.gridfs.GridFS(db, "profilePics")
+
+  def uploadProfilePic(email : String) = Action(gridFSBodyParser(gridFS)) {
+    request =>
+      val futureUpload = for {
+        profileResults <- request.body.files.head.ref
+        result <- users.update(Json.obj("email" -> email), Json.obj("$set" -> Json.obj("profilePic" -> Json.obj("_id" -> BSONFormats.toJSON(profileResults.id)))))
+      } yield result
+
+      Async {
+        futureUpload.map {
+          case _ =>
+            Ok(Json.obj("results" -> "success"))
+        }.recover {
+          case e =>
+            BadRequest(Json.obj("results" -> "error", "details" -> s"There was a problem with your request: ${e.getMessage}"))
+        }
+      }
+  }
+
+}
